b68d606293b4663661c48a7d8f0dcc03ba20ad56
wallok <62080693+wallok@users.noreply.github.com>
2021-05-22 23:10:53 -0500

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,35 @@
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    user={'username': 'miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author':{'username':'Francisco'},
+            'body':'(͡• ͜ʖ ͡•)'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route("/login",methods=["GET", "POST"])
+@app.route("/index/login",methods=["GET", "POST"])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        flash("login reuested for user {},remeber_me={}".format(form.username.data, form.remeber_me.data))
+        return redirect(url_for("index"))
+    return render_template("login.html",title="Iniciar Sesion",form=form)
+
